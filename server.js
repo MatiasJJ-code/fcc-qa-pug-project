ef697f311c99a80f5c6bdbf03b65372b152eeefe
Matias <matiasjauregui@gmail.com>
2025-12-10 20:45:46 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 'use strict';
-require('dotenv').config();
 const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Configurar Pug
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 
-app.route('/').get((req, res) => {
-
+// Ruta PRINCIPAL
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
+// Escuchar
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log('Server running on port ' + PORT);
 });
