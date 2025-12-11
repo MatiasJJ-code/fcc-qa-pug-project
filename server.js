62b8f5f7d605c6d4cc086969b5750d0bb7b459cf
Matias <matiasjauregui@gmail.com>
2025-12-10 22:40:24 -0300

    Add CORS support to the server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 'use strict';
 require('dotenv').config();
+const cors = require('cors');
+app.use(cors());
+
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
