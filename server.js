9eef282c33a6e1908d05f81787c5f33e5ee1f4d8
Matias <matiasjauregui@gmail.com>
2025-12-10 22:24:12 -0300

    Refactor server.js for cleaner setup and logging

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 'use strict';
+require('dotenv').config();
 const express = require('express');
-const app = express();
-
-/****************************************************
- *  FREE CODE CAMP TESTS - DO NOT EDIT OR REMOVE   *
- ***************************************************/
-
+const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
-fccTesting(app);
 
-/****************************************************
- *  SETUP                                           *
- ***************************************************/
+const app = express();
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
+fccTesting(app); // For fCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
   res.render('index');
@@ -22,5 +19,5 @@ app.route('/').get((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
